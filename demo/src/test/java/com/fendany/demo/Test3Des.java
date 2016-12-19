fe5549b484b4b01d26c20946da9e5e5148e29f1c
moilions <moilions@moilions.local>
2016-12-19 15:42:34 +0800

    feat:郑州中心医院测试程序

--- demo/src/test/java/com/fendany/demo/Test3Des.java
+++ demo/src/test/java/com/fendany/demo/Test3Des.java
@@ -1,10 +1,10 @@
 package com.fendany.demo;
 
-import com.alibaba.fastjson.JSON;
 import com.fendany.utils.http.HttpFactory;
 import com.fendany.utils.security.Des3Pro;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -26,23 +26,14 @@ public class Test3Des {
 
     private String url = "http://211.167.232.2:9898/ins/user/getUserFile";
 
-    private String msg = "{\"package\":{\"additionInfo\":" +
-            "{\"asyncAsk\":\"0\",\"callback\":\"\",\"correlationId\":\"\",\"curDllAddr\":\"10.180.193.46:4000\",\"errorCode\":\"\",\"errorMsg\":\"\",\"receiverTradeNum\":\"20161116110029-11101-1159\"}," +
-            "\"body\":[{\"credentialNum\":\"\",\"credentialType\":\"\",\"inHospitalNum\":\"500461899\",\"name\":\"古菊莲\",\"pageNum\":\"1\",\"treatBeginDate\":\"20161001\",\"treatEndDate\":\"20161108\"}]," +
-            "\"head\":{\"busenissType\":\"2\",\"busseID\":\"C210\",\"clientmacAddress\":\"AE:AC:BD:1A:C3\",\"hosorgName\":\"测试粒\",\"hosorgNum\":\"00001\",\"intermediaryCode\":\"\",\"intermediaryName\":\"\",\"receiverCode\":\"400001431\",\"receiverName\":\"郑州市中心医院\",\"recordCount\":\"10\",\"sendTradeNum\":\"20160621889560-11111111-123\",\"senderCode\":\"200000148\",\"senderName\":\"中国平安财产保险股份有限公司\",\"standardVersionCode\":\"1.0.0\",\"systemType\":\"1\"}}}";
-
-    private String msg1 = "{\"package\":{\"additionInfo\":{\"asyncAsk\":\"0\",\"callback\":\"\",\"correlationId\":\"\",\"curDllAddr\":\"10.180.193.46:4000\",\"errorCode\":\"\",\"errorMsg\":\"\",\"receiverTradeNum\":\"20161116153028-11101-1236\"},\"body\":[{\"credentialNum\":\"\",\"credentialType\":\"\",\"inHospitalNum\":\"592700223\",\"name\":\"韦子萱\",\"pageNum\":\"1\",\"treatBeginDate\":\"20161104\",\"treatEndDate\":\"20161108\"}],\"head\":{\"busenissType\":\"2\",\"busseID\":\"C210\",\"clientmacAddress\":\"AE:AC:BD:1A:C3\",\"hosorgName\":\"测试粒\",\"hosorgNum\":\"00001\",\"intermediaryCode\":\"\",\"intermediaryName\":\"\",\"receiverCode\":\"400001431\",\"receiverName\":\"郑州市中心医院\",\"recordCount\":\"10\",\"sendTradeNum\":\"20160621889560-11111111-123\",\"senderCode\":\"200000148\",\"senderName\":\"中国平安财产保险股份有限公司\",\"standardVersionCode\":\"1.0.0\",\"systemType\":\"1\"}}}";
-
     private String key = "@6eauuUYX7jA@LEYUE100@00";
 
-    private String res = "to+GuYqf+HwOOs3RHYgrkxVV86RVHFrJQgejFGTTPOfvEWn52mLR/faf1GZiq6VDPCMRyGXFrol+IppxImFMvwILcz/qLazDjltDISEtyXwe+q6YEhXxlpg/l/MnEaVdZ320cEtn+SJWGvv8It54YupXBxCEbgntQFO3HhS8UDwyvefZoNOu+g==";
-
-    private String surce = "ihlED7zNznd5MS6vlWi6HzGsmqsbKmn8MlN7yKMeAgBCsrb3CGFrdvRxPU5sqBT9DJHddC0vkr8XDUX7UQq1IyI7hxtBhSy5g9djcjLLZgDHdPwCLt2K1INxKDz3ABXs9PUmnYuqISxjXF3wctmPBBjBdaIneFEqx+sY0i+93wjvT3G1KVoBBzd4n3HL51Oj/ghyuTBkRXT/tCJ46Qsy+PN+ph0c78NrxxTudQER1XS5BCNp4Afk8OhKfiX1wtUhlYJhhdEMACHvJdtTS44AGR3DqGeISYtQa21ALb/PHtuCtnMf2q2Fq721n/Llik3/6tTZ8Ihjw9FBc1CKTrn0ZBUm0CPp6iZVY6Z63WnHHYX3YVhSci6xNlPvfTynJLG2bLjSZ3pBkRi59c91Ly8xr5eq1hRiqRDxXv6WcV0+DBfwwJ6HFKRIuwiM7FLXuwfiZ320cEtn+SJFlYEAd1VnKwlGC8iCemf1HgIvfQMeXz4fw/Tku1v07XyjQWfEcE7jX3JiJZlVSvYTG7Rrj3xCNE9nayFLVFkQOf5/k8Jy+n9xUw1um09WcEJ9wJiKlBtWEZaFDcbXBLofqqd6tDC8GMM2yD6DoOOU3XaaFxTED2Yp7NrDrLEW4HnuZIlZHUCMrjsqmrkXMbHZsfWR6i2nupszlZ7WHAKO8kMUeXrFIlOwrn/1n3iF1i+ZoIYWxhSf7t4b00o+Y2zFdCV2xuvLsf2cizVhAFjMlGWkHC3b2o1fg9B1u0ImjCGxeWD9AZoTgJJjKfHvZ7rKVdou/vNdsZyA2zIMyjVHyZ4laUpbi+odSjW4OcrMEQtiqLabCjGKS940eMoEjMpxpdljoM8bF2Xt0zr5DNUW4/7sDIL2v5bu3hvTSj5jbElK3gi8LW4G/iiTUSBH8p5DSUXd4SyR/mybAjJ7FHeUGntqfAs5fxL6uJ5Jc2qrF/nMie/KUm67o+UTifGF2QKI8zNaKPgtDyo7P0Syf/MjlN5u8ihiywxSo7rAyB4RsJ4GKyibgBq0tHfILpWE2I7IUYlQfGnBEw==\n";
-
-    private String sur1 = "ihlED7zNznd5MS6vlWi6HzGsmqsbKmn8MlN7yKMeAgBCsrb3CGFrdvRxPU5sqBT9DJHddC0vkr8XDUX7UQq1IyI7hxtBhSy5g9djcjLLZgDHdPwCLt2K1INxKDz3ABXs9PUmnYuqISxjXF3wctmPBBjBdaIneFEqx+sY0i+93wjvT3G1KVoBBzd4n3HL51Oj/ghyuTBkRXT/tCJ46Qsy+PN+ph0c78NrxxTudQER1XS5BCNp4Afk8OhKfiX1wtUhlYJhhdEMACHvJdtTS44AGR3DqGeISYtQa21ALb/PHtuCtnMf2q2Fq721n/Llik3/6tTZ8Ihjw9FBc1CKTrn0ZBUm0CPp6iZVY6Z63WnHHYX3YVhSci6xNlPvfTynJLG2bLjSZ3pBkRi59c91Ly8xr5eq1hRiqRDxXv6WcV0+DBfwwJ6HFKRIuwiM7FLXuwfiZ320cEtn+SJFlYEAd1VnKwlGC8iCemf1HgIvfQMeXz4fw/Tku1v07XyjQWfEcE7jX3JiJZlVSvYTG7Rrj3xCNE9nayFLVFkQOf5/k8Jy+n9xUw1um09WcEJ9wJiKlBtWEZaFDcbXBLofqqd6tDC8GMM2yD6DoOOU3XaaFxTED2Yp7NrDrLEW4HnuZIlZHUCMrjsqmrkXMbHZsfWR6i2nupszlZ7WHAKO8kMUeXrFIlOwrn/1n3iF1i+ZoIYWxhSf7t4b00o+Y2zFdCV2xuvLsf2cizVhAFjMlGWkHC3b2o1fg9B1u0ImjCGxeWD9AZoTgJJjKfHvZ7rKVdou/vNdsZyA2zIMyjVHyZ4laUpbi+odSjW4OcrMEQtiqLabCjGKS940eMoEjMpxpdljoM8bF2Xt0zr5DNUW4/7sDIL2v5bu3hvTSj5jbElK3gi8LW4G/iiTUSBH8p5DSUXd4SyR/mybAjJ7FHeUGntqfAs5fxL6uJ5Jc2qrF/nMie/KUm67o+UTifGF2QKI8zNaKPgtDyo7P0Syf/MjlN5u8ihiywxSo7rAyB4RsJ4GKyibgBq0tHfILpWE2I7IUYlQfGnBEw==\n";
-
     @Test
-    public void test00() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+    public void testc210() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        call(getMsg());
+    }
+
+    private String call(String msg) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         HttpFactory httpFactory = new HttpFactory();
         httpFactory.init();
         CloseableHttpClient closeableHttpClient = httpFactory.get();
@@ -67,74 +58,23 @@ public class Test3Des {
             System.out.println("【 解码后 】" + result_str);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-    }
-
-    @Test
-    public void test01() {
-//        byte[] key_byte = Base64.decodeBase64(key);
-        byte[] key_byte = key.getBytes();
-        System.out.println("【KEY_LEN】" + key_byte.length);
-        System.out.println("【 解码前 】" + surce);
-        byte[] res_byte = Base64.decodeBase64(surce);
-        byte[] result = Des3Pro.decrypt(key_byte, res_byte);
-        String result_str = new String(result);
-        System.out.println("【 解码后 】" + result_str);
-        System.out.println("【 name  】" + JSON.parseObject(result_str).get("name"));
-    }
-
-    private static class Result {
-
-        private String code;
-
-        private String message;
-
-        private String data;
-
-        public String getCode() {
-            return code;
-        }
-
-        public void setCode(String code) {
-            this.code = code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public String getData() {
-            return data;
-        }
-
-        public void setData(String data) {
-            this.data = data;
-        }
-
-        @Override
-        public String toString() {
-            return "Result{" +
-                    "code='" + code + '\'' +
-                    ", message='" + message + '\'' +
-                    ", data='" + data + '\'' +
-                    '}';
-        }
+        return null;
     }
 
     private String getMsg(){
         return "{\n" +
                 "            \"package\": {\n" +
                 "                \"head\": {\n" +
-                "                    \"busseID\": \"1800\",\n" +
+                "                    \"busseID\": \"C210\",\n" +
                 "                    \"sendTradeNum\": \"20150701083030-10011001-0001\",\n" +
-                "                    \"senderCode\": \"001\",\n" +
+                "                    \"senderCode\": \"100000001\",\n" +
                 "                    \"senderName\": \"平安保险公司\",\n" +
-                "                    \"receiverCode\": \"200000042\",\n" +
+                "                    \"receiverCode\": \"400001431\",\n" +
                 "                    \"receiverName\": \"郑州中心医院\",\n" +
                 "                    \"intermediaryCode\": \"003\",\n" +
                 "                    \"intermediaryName\": \"乐约健康\",\n" +
@@ -146,7 +86,13 @@ public class Test3Des {
                 "                    \"clientmacAddress \": \"30BB7E0A5E2D \",\n" +
                 "                    \" recordCount \": \"1\"\n" +
                 "                },\n" +
-                "                \"body\": [{\"credentialType\":\"1\",\"credentialNum\":\"341221198903048135\",\"name\":\"\\u5218\\u5e86\\u8f89\",\"beginDate\":\"20161110\",\"endDate\":\"20161210\"}\n" +
+                "                \"body\": [" +
+                "                       {   \"inHospitalNum\":\"500755113\"," +
+                "                           \"pageNum\":\"1\"," +
+                "                           \"name\":\"万朝峰\"" +
+//                "                           ,\"treatBeginDate\":\"20160914\"," +
+//                "                           \"treatEndDate\":\"20160916\"" +
+                "                       }\n" +
                 "                ],\n" +
                 "                \"additionInfo\": {\n" +
                 "                    \"errorCode\": \"0\",\n" +
--- demo/src/test/java/com/fendany/demo/TestJava.java
+++ demo/src/test/java/com/fendany/demo/TestJava.java
@@ -3,14 +3,84 @@ package com.fendany.demo;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by zuoer on 16-9-27.
  */
 public class TestJava {
 
     @Test
-    public void test00(){
+    public void test00() {
         String temp = ",,,,,,,,,a,b,c,,,,,,";
         System.out.println(ArrayUtils.toString(temp.split(",")));
+    }
+
+
+    @Test
+    public void test01() {
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        List<String> list2 = new ArrayList<>();
+        list2.add("a");
+        list2.add("b");
+        list2.add("c");
+
+        for (String temp : list2) {
+            if (!list1.contains(temp)) {
+                System.out.println(temp);
+            }
+        }
+
+
+        List<emp> list1_ = new ArrayList<>();
+        list1_.add(new emp("a"));
+        list1_.add(new emp("b"));
+        List<emp> list2_ = new ArrayList<>();
+        list2_.add(new emp("a"));
+        list2_.add(new emp("b"));
+        list2_.add(new emp("c"));
+        List<String> list3 = new ArrayList<>();
+        for (emp e : list1_) {
+            list3.add(e.getX());
+        }
+
+        List<emp> result = new ArrayList<>();
+        for (emp e : list2_) {
+            String temp = e.getX();
+            if (temp != null && !list3.contains(temp) ) {
+                result.add(e);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+
+    private class emp {
+
+        private String x;
+
+        public emp(String name) {
+            x = name;
+        }
+
+        public String getX() {
+            return x;
+        }
+
+        public void setX(String x) {
+            this.x = x;
+        }
+
+        @Override
+        public String toString() {
+            return "emp{" +
+                    "x='" + x + '\'' +
+                    '}';
+        }
     }
 }
