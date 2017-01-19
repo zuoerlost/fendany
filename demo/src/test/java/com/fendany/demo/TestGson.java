6a3fc5a3c1ad909a71ab014268ee9be93887b1fb
moilions <moilions@moilions.local>
2017-01-19 16:10:13 +0800

    feat:gson

---
+++ demo/src/test/java/com/fendany/demo/TestGson.java
@@ -0,0 +1,34 @@
+package com.fendany.demo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.junit.Test;
+
+/**
+ * Created by moilions on 2017/1/19.
+ */
+public class TestGson {
+
+    @Test
+    public void test01() {
+        String uglyJSONString = "{\"data1\":100,\"data2\":\"hello\",\"list\":[\"String 1\",\"String 2\",\"String 3\"]}";
+        String prettyJsonString = jsonFormatter(uglyJSONString);
+        System.out.println("JSON格式化前：");
+        System.out.println(uglyJSONString);
+        System.out.println("JSON格式化后：");
+        System.out.println(prettyJsonString);
+
+    }
+
+    public static String jsonFormatter(String uglyJSONString){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(uglyJSONString);
+        String prettyJsonString = gson.toJson(je);
+        return prettyJsonString;
+    }
+
+
+}
--- demo/src/test/java/com/fendany/demo/TestleyueJIangbei.java
+++ demo/src/test/java/com/fendany/demo/TestleyueJIangbei.java
@@ -1,79 +1,27 @@
 package com.fendany.demo;
 
-import com.fendany.utils.http.HttpFactory;
-import com.fendany.utils.security.Des3Pro;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by moilions on 2016/11/9.
- * <p>
- * pid=7vl5qiJf0kt&iid=6eauuUYX7jA&hid=j5PR7kfSs4n
  */
-public class Testleyue {
-
-    /**
-     * http://182.92.192.186:8080
-     */
-    public static String c210 = "http://211.167.232.2:9898/ins/user/getUserFile";
-
-    public static String c220 = "http://211.167.232.2:9898/ins/User/confirm";
-
-    public static String key = "@6eauuUYX7jA@LEYUE100@00";
+public class TestleyueJIangbei {
 
     @Test
     public void testc210() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        call(getC210(), c210);
+        Testleyue.call(getC210(),Testleyue.c210);
     }
 
     @Test
     public void testc220() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        call(getC220("Z10045494"), c220);
+        Testleyue.call(getC220("Z10045494"),Testleyue.c220);
     }
 
-    public static String call(String msg, String url) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        HttpFactory httpFactory = new HttpFactory();
-        httpFactory.init();
-        CloseableHttpClient closeableHttpClient = httpFactory.get();
-        HttpPost httpPost = new HttpPost(url);
-        byte[] key_byte = key.getBytes();
-        try {
-            byte[] msg_byte = Des3Pro.encrypt(key_byte, msg.getBytes());
-            String req = Base64.encodeBase64String(msg_byte);
-            System.out.println("【加密前】" + TestGson.jsonFormatter(msg));
-            System.out.println("【加密后】" + req);
-            StringEntity input = new StringEntity(req);
-            httpPost.setEntity(input);
-            HttpResponse response = closeableHttpClient.execute(httpPost);
-            String result = EntityUtils.toString(response.getEntity());
-            System.out.println("【 解码前 】" + result);
-            byte[] res_byte = Base64.decodeBase64(result);
-            byte[] result_byte = Des3Pro.decrypt(key_byte, res_byte);
-            String result_str = new String(result_byte);
-            System.out.println("【 解码后 】" + TestGson.jsonFormatter(result_str));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String getC210() {
+    private String getC210(){
         return "{\n" +
                 "            \"package\": {\n" +
                 "                \"head\": {\n" +
@@ -81,8 +29,8 @@ public class Testleyue {
                 "                    \"sendTradeNum\": \"20150701083030-10011001-0001\",\n" +
                 "                    \"senderCode\": \"100000001\",\n" +
                 "                    \"senderName\": \"平安保险公司\",\n" +
-                "                    \"receiverCode\": \"400001431\",\n" +
-                "                    \"receiverName\": \"郑州中心医院\",\n" +
+                "                    \"receiverCode\": \"400012974\",\n" +
+                "                    \"receiverName\": \"南京江北人民医院\",\n" +
                 "                    \"intermediaryCode\": \"003\",\n" +
                 "                    \"intermediaryName\": \"乐约健康\",\n" +
                 "                    \"hosorgNum\": \"001\",\n" +
@@ -94,11 +42,11 @@ public class Testleyue {
                 "                    \" recordCount \": \"1\"\n" +
                 "                },\n" +
                 "                \"body\": [" +
-                "                       {   \"inHospitalNum\":\"500753964\"," +
+                "                       {   \"inHospitalNum\":\"201618983\"," +
                 "                           \"pageNum\":\"1\" " +
-                "                           ,\"name\":\"高文雅\"" +
-//                "                           ,\"treatBeginDate\":\"20160914\"," +
-//                "                           \"treatEndDate\":\"20160916\"" +
+                "                           ,\"name\":\"吴超英\"" +
+                "                           ,\"treatBeginDate\":\"20160904\"," +
+                "                           \"treatEndDate\":\"20160906\"" +
                 "                       }\n" +
                 "                ],\n" +
                 "                \"additionInfo\": {\n" +
@@ -114,7 +62,7 @@ public class Testleyue {
                 "        }";
     }
 
-    private String getC220(String medicalNum) {
+    private String getC220(String medicalNum){
         return "{\n" +
                 "            \"package\": {\n" +
                 "                \"head\": {\n" +
@@ -122,8 +70,8 @@ public class Testleyue {
                 "                    \"sendTradeNum\": \"20150701083030-10011001-0001\",\n" +
                 "                    \"senderCode\": \"100000001\",\n" +
                 "                    \"senderName\": \"平安保险公司\",\n" +
-                "                    \"receiverCode\": \"400001431\",\n" +
-                "                    \"receiverName\": \"郑州中心医院\",\n" +
+                "                    \"receiverCode\": \"400012974\",\n" +
+                "                    \"receiverName\": \"南京江北人民医院\",\n" +
                 "                    \"intermediaryCode\": \"003\",\n" +
                 "                    \"intermediaryName\": \"乐约健康\",\n" +
                 "                    \"hosorgNum\": \"001\",\n" +
@@ -135,7 +83,7 @@ public class Testleyue {
                 "                    \" recordCount \": \"1\"\n" +
                 "                },\n" +
                 "                \"body\": [" +
-                "                       {   \"medicalNum\":\"" + medicalNum + "\" \n," +
+                "                       {   \"medicalNum\":\""+medicalNum+"\" \n," +
                 "                           \"affirmFlg\":\"1\" \n" +
                 "                           ,\"imageFlg\":\"0\" \n" +
                 "                       }\n" +
